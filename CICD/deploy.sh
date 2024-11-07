ebc1e77a188a969a427332c472bd29780d4d4b6e
Matthew Davis <github@mdavis.xyz>
2024-11-07 21:40:06 +0100

    fix wrong lambda name in CICD

--- CICD/deploy.sh
+++ CICD/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#set -e
+set -e
 set -o pipefail
 export AWS_DEFAULT_REGION=ap-southeast-2
-export LAMBDA_FUNCTION=test-lammulti
+export LAMBDA_FUNCTION="${LAMBDA_FUNCTION:test-lammulti}"
 test $LAMBDA_FUNCTION
 cd lambda_code
 pip install ../../ --target ./
@@ -12,7 +12,7 @@ echo deploying code
 aws lambda update-function-code --function-name $LAMBDA_FUNCTION --zip-file fileb://$(pwd)/../code.zip
 echo "waiting for lambda to finalize"
 aws lambda wait function-updated --function-name $LAMBDA_FUNCTION
-echo "invoking"
+echo "invoking $LAMBDA_FUNCTION"
 RESULT=$(aws lambda invoke --function-name $LAMBDA_FUNCTION --invocation-type RequestResponse RequestResponse --log-type Tail)
 echo $RESULT | jq .
 test null = $(echo $RESULT | jq .FunctionError)
